39bb49660ffd10600602e9b02120511ebd45dfdb
kitaone <mat.mikita@gmail.com>
2018-01-29 17:33:32 +0100

    Odcinek 6 - zakończony

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class BuildManager : MonoBehaviour {
+
+    public static BuildManager instance;
+
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Debug.Log("More then one build maganer in scene");
+            return;
+        }
+        instance = this;
+    }
+
+    public GameObject standardTurretPrefab;
+
+    private void Start()
+    {
+        turretToBuild = standardTurretPrefab;
+    }
+
+    private GameObject turretToBuild;
+
+    public GameObject GetTurretToBuild()
+    {
+        return turretToBuild;
+    }
+}
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Node : MonoBehaviour {
+
+    public Color hoverColor;
+
+    private GameObject turret;
+
+    private Renderer rend;
+    private Color startColor;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+    }
+
+    void OnMouseDown()
+    {
+        if(turret != null)
+        {
+            Debug.Log("Can't build there! - TODO: Display on screen");
+            return;
+        }
+
+        GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
+        turret =(GameObject) Instantiate(turretToBuild, transform.position, transform.rotation);
+    }
+
+    void OnMouseEnter()
+    {
+        GetComponent<Renderer>().material.color = hoverColor;
+    }
+
+    void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+}
